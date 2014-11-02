18404d3a70f38ccef5e75f35c9f079af4cd9dcbe
Yamamoto <shibainu_dan@yahoo.co.jp>
2014-11-02 23:00:45 +0900

    made desktop

---
+++ desktop/Cubemeiro_Project/Assets/Scripts/PlayerCtr.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCtr : MonoBehaviour {
+    const float RayCastMaxDistance = 100.0f;
+    InputManager inputManager;
+    public float attackRange = 1.5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (inputManager.Clicked())
+        {
+            // RayCastで対象物を調べる.
+            Ray ray = Camera.main.ScreenPointToRay(inputManager.GetCursorPosition());
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo, RayCastMaxDistance, (1 << LayerMask.NameToLayer("Ground")) | (1 << LayerMask.NameToLayer("EnemyHit"))))
+            {
+                // 地面がクリックされた.
+                if (hitInfo.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                    SendMessage("SetDestination", hitInfo.point);
+                
+            }
+        }
+	}
+}
